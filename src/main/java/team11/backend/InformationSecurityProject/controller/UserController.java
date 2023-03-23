5ba870cde3444271e3fb2a5361e62d7b03c45d1c
ForLoop111 <vlada.devic.2001@gmail.com>
2023-03-23 10:35:57 +0100

    Added user login

--- src/main/java/team11/backend/InformationSecurityProject/controller/UserController.java
+++ src/main/java/team11/backend/InformationSecurityProject/controller/UserController.java
@@ -5,13 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team11.backend.InformationSecurityProject.dto.LoginCredentials;
+import team11.backend.InformationSecurityProject.dto.TokenStateOut;
 import team11.backend.InformationSecurityProject.dto.UserIn;
 import team11.backend.InformationSecurityProject.dto.UserOut;
+import team11.backend.InformationSecurityProject.exceptions.BadRequestException;
 import team11.backend.InformationSecurityProject.model.StandardUser;
+import team11.backend.InformationSecurityProject.model.User;
+import team11.backend.InformationSecurityProject.security.RefreshTokenService;
+import team11.backend.InformationSecurityProject.security.TokenUtils;
 import team11.backend.InformationSecurityProject.service.StandardUserService;
 
 @RestController
@@ -20,10 +31,17 @@ public class UserController {
 
     private final StandardUserService standardUserService;
 
+    private final AuthenticationManager authenticationManager;
+    private final TokenUtils tokenUtils;
+    private final RefreshTokenService refreshTokenService;
+
     @Autowired
-    public UserController(StandardUserService standardUserService){
+    public UserController(StandardUserService standardUserService, AuthenticationManager authenticationManager, TokenUtils tokenUtils, RefreshTokenService refreshTokenService){
 
         this.standardUserService = standardUserService;
+        this.authenticationManager = authenticationManager;
+        this.tokenUtils = tokenUtils;
+        this.refreshTokenService = refreshTokenService;
     }
     @PostMapping(
             value = "/register",
@@ -33,6 +51,26 @@ public class UserController {
         StandardUser user = standardUserService.register(userDTO);
         UserOut userOut = new UserOut(user);
         return new ResponseEntity<>(userOut, HttpStatus.OK);
+    }
+
+    @PostMapping(
+            value = "/login",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<TokenStateOut> login(@RequestBody @Valid LoginCredentials credentials){
+        Authentication authentication;
+        try {
+             authentication = this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(credentials.getEmail(), credentials.getPassword()));
+
+        }catch (AuthenticationException e){
+            throw new BadRequestException("Wrong email or password");
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        User user = (User) authentication.getPrincipal();
+        String jwt = this.tokenUtils.generateToken(user);
+        String refreshToken = this.refreshTokenService.createRefreshToken(user).getToken();
+        return new ResponseEntity<>(new TokenStateOut(jwt, refreshToken), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/team11/backend/InformationSecurityProject/dto/LoginCredentials.java
@@ -0,0 +1,28 @@
+package team11.backend.InformationSecurityProject.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginCredentials {
+    @NotBlank(message = "Field (email) is required")
+    @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+    private String email;
+
+    @NotBlank(message = "Field (password) is required")
+    @Size(min=6,max = 30,message = "Field (password) cannot be less than 6 characters and more than 30 characters long")
+    private String password;
+
+    public LoginCredentials() {
+        super();
+    }
+
+    public LoginCredentials(String email, String password) {
+        this.setEmail(email);
+        this.setPassword(password);
+    }
+}
