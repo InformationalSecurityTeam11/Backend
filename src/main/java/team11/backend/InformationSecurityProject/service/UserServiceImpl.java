e33cad1519e90eda61f510e675e530ec97ea2c46
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-03-25 18:23:57 +0100

    light changes

--- src/main/java/team11/backend/InformationSecurityProject/service/UserServiceImpl.java
+++ src/main/java/team11/backend/InformationSecurityProject/service/UserServiceImpl.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Service;
 import team11.backend.InformationSecurityProject.exceptions.NotFoundException;
 import team11.backend.InformationSecurityProject.model.User;
 import team11.backend.InformationSecurityProject.repository.UserRepository;
-import team11.backend.InformationSecurityProject.service.interfaces.UserService;
+import team11.backend.InformationSecurityProject.service.interfaces.IUserService;
 
 import java.util.Optional;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements IUserService {
 
     private final UserRepository userRepository;
 
