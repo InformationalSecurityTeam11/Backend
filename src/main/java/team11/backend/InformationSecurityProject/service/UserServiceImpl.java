aa6206712224d86c6189f603ab0a534b88fa3498
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-03-25 18:08:42 +0100

    deleted unused lines

--- src/main/java/team11/backend/InformationSecurityProject/service/UserServiceImpl.java
+++ src/main/java/team11/backend/InformationSecurityProject/service/UserServiceImpl.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Service;
 import team11.backend.InformationSecurityProject.exceptions.NotFoundException;
 import team11.backend.InformationSecurityProject.model.User;
 import team11.backend.InformationSecurityProject.repository.UserRepository;
+import team11.backend.InformationSecurityProject.service.interfaces.UserService;
 
 import java.util.Optional;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
