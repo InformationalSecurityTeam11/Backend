86e858d1a610ba894a53ec7a10a3495005306ad8
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-03-25 18:23:41 +0100

    rename UserService interface to IUserService

--- src/main/java/team11/backend/InformationSecurityProject/service/interfaces/IUserService.java
+++ src/main/java/team11/backend/InformationSecurityProject/service/interfaces/IUserService.java
@@ -2,6 +2,6 @@ package team11.backend.InformationSecurityProject.service.interfaces;
 
 import team11.backend.InformationSecurityProject.model.User;
 
-public interface UserService {
+public interface IUserService {
     User getUser(Integer id);
 }
