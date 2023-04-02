748aa556a0b85eea2b01e2094fdce67fc6d38fa7
ForLoop111 <vlada.devic.2001@gmail.com>
2023-04-02 14:15:09 +0200

    data.sql bugfix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,9 @@ INSERT INTO ROLES (name) VALUES('ROLE_STANDARD');
 
 
 INSERT INTO USERS (user_type, email, last_password_reset_date, name, password, surname, telephone_number, role_id)
-VALUES ('STANDARD','aleksandrab024@hotmail.com', '2023-03-01T04:49:27Z', 'Aleksandra', '$2a$10$iZZWXRF1TFvTNwq9pobYRO8SmuQ9ALEDGtEy4iXNG.bihvOwEJttu', 'Balazevic', '0600538922', '2'),
-       ('STANDARD','vladadevic@gmail.com', '2023-03-01T04:49:27Z', 'Vlada', '$2a$10$iZZWXRF1TFvTNwq9pobYRO8SmuQ9ALEDGtEy4iXNG.bihvOwEJttu', 'Devic', '0613191670', '2'),
-       ('ADMIN','veljkovex@gmail.com', '2023-03-01T04:49:27Z', 'Veljko', '$2a$10$iZZWXRF1TFvTNwq9pobYRO8SmuQ9ALEDGtEy4iXNG.bihvOwEJttu', 'Bubnjevic', '0612638823', '1');
+VALUES ('STANDARD','aleksandrab024@hotmail.com', '2023-04-01T04:49:27Z', 'Aleksandra', '$2a$10$iZZWXRF1TFvTNwq9pobYRO8SmuQ9ALEDGtEy4iXNG.bihvOwEJttu', 'Balazevic', '0600538922', '2'),
+       ('STANDARD','vladadevic@gmail.com', '2023-04-01T04:49:27Z', 'Vlada', '$2a$10$iZZWXRF1TFvTNwq9pobYRO8SmuQ9ALEDGtEy4iXNG.bihvOwEJttu', 'Devic', '0613191670', '2'),
+       ('ADMIN','veljkovex@gmail.com', '2023-04-01T04:49:27Z', 'Veljko', '$2a$10$iZZWXRF1TFvTNwq9pobYRO8SmuQ9ALEDGtEy4iXNG.bihvOwEJttu', 'Bubnjevic', '0612638823', '1');
 
-
+-- Ako ne radi login na preloaded korisnike probaj da promenis last_password_reset_date na noviji datum
 -- password: 123456 -> $2a$10$iZZWXRF1TFvTNwq9pobYRO8SmuQ9ALEDGtEy4iXNG.bihvOwEJttu
