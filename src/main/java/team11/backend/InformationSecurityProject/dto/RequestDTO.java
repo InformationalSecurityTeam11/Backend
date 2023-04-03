631aefbcc99039f615d5ac9a5d74923f5fb9e3b2
ForLoop111 <vlada.devic.2001@gmail.com>
2023-04-03 11:29:12 +0200

    Resolved conflict

--- src/main/java/team11/backend/InformationSecurityProject/dto/RequestDTO.java
+++ src/main/java/team11/backend/InformationSecurityProject/dto/RequestDTO.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import team11.backend.InformationSecurityProject.model.CertificateRequest;
 import team11.backend.InformationSecurityProject.model.CertificateType;
-import team11.backend.InformationSecurityProject.model.User;
 
 import java.time.LocalDateTime;
 
@@ -15,7 +14,7 @@ public class RequestDTO {
     private Integer id;
     private CertificateInfoDTO certificateParent;
     private LocalDateTime creationTime;
-    private Boolean isAccepted;
+    private String requestState;
     private LocalDateTime acceptanceTime;
     private UserInfoDTO owner;
     private CertificateType certificateType;
@@ -26,7 +25,7 @@ public class RequestDTO {
         this.id = request.getId();
         this.certificateParent = new CertificateInfoDTO(request.getParent());
         this.creationTime = request.getCreationTime();
-        this.isAccepted = request.getIsAccepted();
+        this.requestState = request.getRequestState().toString();
         this.owner = new UserInfoDTO(request.getOwner());
         this.certificateType = request.getCertificateType();
         this.organization = request.getOrganization();
